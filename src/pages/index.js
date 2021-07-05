54a9f6c86b1ec570817bc782a0c5feee7d7551ba
erin <elemkey@gmail.com>
2021-07-05 12:01:45 -0400

    map and stars on page issues z-index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,19 @@
-import * as React from "react"
-import Layout from '../components/layout'
+import * as React from "react";
+import Layout from "../components/layout";
+import TrainMap from "./TrainMap";
 
 // markup
 const IndexPage = () => {
   return (
     <>
-    <Layout>
-    <div className="stars"></div>
-        <div className="twinkling"></div> 
-       <div className="clouds"></div>
-       </Layout>
-     </>
-  )
-}
+      <Layout>
+      <TrainMap />
+        <div className="stars"></div>
+        <div className="twinkling"></div>
+        <div className="clouds"></div>
+      </Layout>
+    </>
+  );
+};
 
-export default IndexPage
+export default IndexPage;
