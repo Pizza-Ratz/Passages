2ebac4347d57aec3fa86ee852101fa7f7af11982
Catherine Mitchell <16630303+gynnantonix@users.noreply.github.com>
2021-07-06 19:31:24 -0400

    proves ability to extract events from SVG

--- App/src/pages/TrainMap.js
+++ App/src/pages/TrainMap.js
@@ -1,20 +1,22 @@
-import React, { Component } from 'react';
-import mapUrl, { ReactComponent as Map } from '../images/map-detail-transparent.svg';
-import Layout from '../components/layout'
+import React from 'react';
+import mapUrl from '../images/map-detail-transparent.svg';
 
-export default class TrainMap extends Component {
-
-
-  render(){
-
-   
-    return (
-      <Layout>
-      <div className="train-container">
-      <Map />
-      </div>
-      </Layout>
-
-    )
+const TrainMapSVG = (props) => {
+  const handleClick = (evt) => {
+    console.log(evt)
   }
+
+  const imgRef = React.useRef()
+
+  React.useEffect(() => {
+    if (imgRef.current.contentDocument) {
+      imgRef.current.contentDocument.getElementById('polyline2792-2').addEventListener('mouseover', console.log)
+    }
+  })
+
+  return (
+    <object ref={imgRef} id="svg-object" data={mapUrl} type="image/svg+xml"></object>
+  )
 }
+
+export default TrainMapSVG
