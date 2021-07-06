4c5d401e60aead06579f5b5bfc71a54e56c483f9
Ella Heron <heronsound95@gmail.com>
2021-07-06 10:44:44 -0400

    adds tone.js mono synth and sliders to demo

--- src/index.js
+++ src/index.js
@@ -6,6 +6,62 @@ const baseNote = teoria.note(`${line}4`)
 const scale = baseNote.scale('lydian')
 
 /**
+ * Tone.js demo
+ */
+
+ const noteMapping = {
+  a: 'C4',
+  w: 'C#4',
+  s: 'D4',
+  e: 'D#4',
+  d: 'E4',
+  f: 'F4',
+  t: 'F#4',
+  g: 'G4',
+  y: 'G#4',
+  h: 'A4',
+  u: 'A#4',
+  j: 'B4',
+  k: 'C5'
+}
+
+// Create a dictionary of keys being held down to prevent automatic key re-firing
+const pressedKeys = {};
+
+const synth = new Tone.MonoSynth();
+const dist = new Tone.Distortion(0.5);
+const vol = new Tone.Volume(0);
+synth.chain(dist, vol, Tone.Destination);
+
+const distSlider = document.getElementById('distortion');
+const volSlider = document.getElementById('volume');
+
+document.addEventListener('keydown', evt => {
+  // evt.code is always formatted as 'KeyA', 'KeyQ', etc.
+  const char = evt.code.slice(3).toLowerCase();
+  if (noteMapping[char] && !pressedKeys[char]) {
+    pressedKeys[char] = true;
+    synth.triggerAttack(noteMapping[char]);
+  }
+})
+
+document.addEventListener('keyup', evt => {
+  const char = evt.code.slice(3).toLowerCase();
+  if (noteMapping[char]) {
+    delete pressedKeys[char];
+    synth.triggerRelease();
+  }
+})
+
+distSlider.onchange = () => {
+  dist.wet.value = Number(distSlider.value) / 100
+}
+
+volSlider.onchange = () => {
+  vol.volume.value = volSlider.value - 60 // offset to set 60 slider value = 0 (unity) gain
+}
+
+/**
  * Given the data for a stop, filters arrival data for selected line and produces
  * quarter-note quantized note data.
  */
